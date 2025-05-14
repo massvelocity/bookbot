22e1ab371a2c9fa82279a002a928021a40f35b73
massvelocity <massvelocity42@.com>
2025-05-13 19:59:07 -0500

    wait this is the real final

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -8,12 +7,11 @@ from stats import (
 
 
 def main():
-    args = sys.argv
-    if len(args) < 2:
+    if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
+    book_path = sys.argv[1]
 
-    book_path = args[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
